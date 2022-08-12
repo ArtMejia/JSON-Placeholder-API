b564f7a2638a8fb227ec05e96c07d476c8cccffb
ArtMejia <arteniomejia.student@careerdevs.com>
2022-08-11 21:15:37 -0400

    added routes

--- src/main/java/com/careerdevs/jsonplaceholder/controllers/UserController.java
+++ src/main/java/com/careerdevs/jsonplaceholder/controllers/UserController.java
@@ -1,23 +1,24 @@
 package com.careerdevs.jsonplaceholder.controllers;
 
-
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
-@RequestMapping ("/api/user")
+@RequestMapping ("/api/users")
 public class UserController {
     private final String jsonPlaceholderEndpoint = "https://jsonplaceholder.typicode.com/posts";
     private final String commentEndPoint = "https://jsonplaceholder.typicode.com/comments";
     private final String photosEndPoint = "https://jsonplaceholder.typicode.com/photos";
     private final String albumsEndPoint = "https://jsonplaceholder.typicode.com/albums";
+    private final String usersEndpoint = "https://jsonplaceholder.typicode.com/users";
 
-    @GetMapping("/posts")
-    public Object postsRoute(RestTemplate restTemplate) {
-        return restTemplate.getForObject(jsonPlaceholderEndpoint, Object.class);
-
+    @GetMapping("/all")
+    public ResponseEntity<?> getUsers (RestTemplate restTemplate) {
+        String response = restTemplate.getForObject(usersEndpoint, String.class);
+        return ResponseEntity.ok(response);
     }
 
     @GetMapping("/comments")
